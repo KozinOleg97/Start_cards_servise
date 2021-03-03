fcf639ab05ba88fe7a9f2ce93722679076485872
KozinOleg97 <olegkosin.dev@yandex.ru>
2021-03-03 18:44:33 +0300

    init

---
+++ src/main/java/org/acme/service/Accounts.java
@@ -0,0 +1,31 @@
+package org.acme.service;
+
+import io.quarkus.panache.common.Sort;
+
+import org.acme.entity.Card;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+@Path("lk")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
+public class Accounts {
+
+    private static final Logger LOGGER = Logger.getLogger(Accounts.class.getName());
+
+
+    @GET
+    public List<Card> get() {
+        return Card.listAll(Sort.by("date"));
+    }
+
+
+}
